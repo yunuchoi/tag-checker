055793d5079ab59e25a2fae53c04185e84e0940d
Yunu Choi <yoonwoochoi00@gmail.com>
2024-03-07 21:53:46 +1300

    Check stack every iteration

--- src/checkTags.ts
+++ src/checkTags.ts
@@ -5,6 +5,10 @@ export function checkTags(paragraph: string): string {
 
     for (const tag of tags) {
         if (tag.startsWith('</')) {
+            if (stack.length === 0) {
+                return `Expected # found ${tag}`
+            }
+            
             // Compare the closing tag with the most recent opening tag
             const openingTag: string = stack[stack.length - 1].substring(1, 2);
             const closingTag: string = tag.substring(2, 3);
