c54e66aaf6b3a3d33598fb27b6649222a0ce5eff
Yunu Choi <yoonwoochoi00@gmail.com>
2024-03-07 22:30:44 +1300

    Add edge test case

--- tests/checkTags.test.ts
+++ tests/checkTags.test.ts
@@ -1,28 +1,33 @@
 import { checkTags } from "../src/checkTags";
 
 describe("Testing checkTags", () => {
-    it("Sample Input 1", () => {
+    it("correctly checks a well-formed paragraph", () => {
         expect(checkTags("The following text <C><B>is centered and in boldface</B></C>"))
         .toEqual("Correctly tagged paragraph");
     });
 
-    it("Sample Input 2", () => {
+    it("handles tags outside of the boundary", () => {
         expect(checkTags("<B>This <\\g>is <B>boldface</B> in <<*> a</B> <\\6> <<d> sentence"))
         .toEqual("Correctly tagged paragraph");
     });
 
-    it("Sample Input 3", () => {
+    it("detects wrongly nested tags", () => {
         expect(checkTags("<B><C>This should be centered and in boldface, but the tags are wrongly nested</B></C>"))
         .toEqual("Expected </C> found </B>");
     });
 
-    it("Sample Input 4", () => {
+    it("detects an extra closing tag without a matching opening tag", () => {
         expect(checkTags("<B>This should be in boldface, but there is an extra closing tag</B></C>"))
         .toEqual("Expected # found </C>");
     });
 
-    it("Sample Input 5", () => {
+    it("detects a missing closing tag required", () => {
         expect(checkTags("<B><C>This should be centered and in boldface, but there is a missing closing tag</C>"))
         .toEqual("Expected </B> found #");
     });
+
+    it("handles a plain text without tags", () => {
+        expect(checkTags("This is a plain text without any tags"))
+        .toEqual("Correctly tagged paragraph")
+    })
 });
