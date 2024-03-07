9524807800e0de26318a0a29a34a945afca7e065
Yunu Choi <yoonwoochoi00@gmail.com>
2024-03-07 21:47:28 +1300

    Implement stack structure

--- src/checkTags.ts
+++ src/checkTags.ts
@@ -1,6 +1,24 @@
 export function checkTags(paragraph: string): string {
     const pattern: RegExp = /<\/?([A-Z])>/g;
     const tags: string[] = paragraph.match(pattern) || [];
+    const stack: string[] = [];
 
-    return "";
+    for (const tag of tags) {
+        if (tag.startsWith('</')) {
+            // Compare the closing tag with the most recent opening tag
+            const openingTag: string = stack[stack.length - 1].substring(1, 2);
+            const closingTag: string = tag.substring(2, 3);
+            
+            if (openingTag === closingTag) {
+                stack.pop();
+            } else {
+                return `Expected </${openingTag}> found </${closingTag}>`
+            }
+        } else {
+            // Add opening tag into the stack
+            stack.push(tag);
+        }
+    }
+
+    return "Correctly tagged paragraph";
 }
