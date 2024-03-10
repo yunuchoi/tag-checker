fb855f83bdcc766ec9c028ad8b1117e85a5a75f4
Yunu Choi <yoonwoochoi00@gmail.com>
2024-03-10 19:27:33 +1300

    Add function descriptions

--- src/validateTagNames.ts
+++ src/validateTagNames.ts
@@ -1,13 +1,32 @@
+/**
+ * Checks if the given tag is a closing tag by examining its format.
+ *
+ * @param {string} tag - The tag to be checked.
+ * @returns {boolean} - True if the tag is a closing tag, otherwise false.
+ */
 function isClosingTag(tag: string): boolean {
     return tag.startsWith('</');
 }
 
+/**
+ * Extracts tags from the given paragraph using a regular expression.
+ *
+ * @param {string} paragraph - The paragraph containing tags.
+ * @returns {string[]} - An array of extracted tags.
+ */
 function extractTags(paragraph: string): string[] {
     const pattern: RegExp = /<\/?([A-Z])>/g;
 
     return paragraph.match(pattern) || [];
 }
 
+/**
+ * Extracts and returns the tag name from the provided tag.
+ * For closing tags, it extracts the second character; for opening tags, it extracts the first character.
+ *
+ * @param {string} tag - The tag to extract the name from.
+ * @returns {string} - The extracted tag name.
+ */
 function extractTagName(tag: string): string {
     if (isClosingTag(tag)) {
         return tag.substring(2, 3);
@@ -16,10 +35,23 @@ function extractTagName(tag: string): string {
     return tag.substring(1, 2);
 }
 
+/**
+ * Checks if the provided opening and closing tags match.
+ *
+ * @param {string} openingTag - The opening tag.
+ * @param {string} closingTag - The closing tag.
+ * @returns {boolean} - True if the tags match, otherwise false.
+ */
 function tagsMatch(openingTag: string, closingTag: string): boolean {
     return openingTag === closingTag;
 }
 
+/**
+ * Validates the correctness of tags pairing within a given paragraph.
+ *
+ * @param {string} paragraph - The paragraph to be validated.
+ * @returns {string} - A validation message indicating the correctness of tag pairing.
+ */
 export function validateTagNames(paragraph: string): string {
     const tags: string[] = extractTags(paragraph);
     const tagStack: string[] = [];
